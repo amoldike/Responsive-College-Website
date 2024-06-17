3d3ba2f4c3808f78233bd7d5c2d5ac39984eb2df
Amol Dike <amoldike002@gmail.com>
2024-06-17 16:55:58 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import Programs from './Components/Programs/Programs'
-import Title from './Components/Title/Title'
-import About from './Components/About/About'
-import Campus from './Components/Campus/Campus'
-import Testimonials from './Components/Testimonials/Testimonials'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import Programs from "./Components/Programs/Programs";
+import Title from "./Components/Title/Title";
+import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
@@ -26,9 +25,8 @@ const App = () => {
         <Contact />
         <Footer />
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,72 @@
-import React, { useEffect, useState } from 'react'
-import "./Navbar.css"
-import logo from "../../assets/logo.png"
-import { Link } from 'react-scroll'
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
+import menuIcon from "../../assets/menu-icon.png";
 const Navbar = () => {
-
-    const [sticky, setSticky] = useState(false)
+    const [sticky, setSticky] = useState(false);
+    const [mobileMenu, setMobileMenu] = useState(false);
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
-            window.scrollY > 50 ? setSticky(true) : setSticky(false)
-        })
-    }, [])
-    return (
-        <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
-            <img src={logo} alt="logo" className='logo' />
-            <ul>
-                <li><Link to="hero" smooth={true} offset={0} duration={500}>Home</Link></li>
-                <li><Link to="program" smooth={true} offset={-260} duration={500}>Program</Link></li>
-                <li><Link to="about" smooth={true} offset={-150} duration={500}>About</Link> US</li>
-                <li><Link to="campus" smooth={true} offset={-240} duration={500}>Campus</Link></li>
-                <li><Link to="testimonials" smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
-                <li><Link to='contact' smooth={true} offset={-210} duration={500} className="btn">Contact US</Link></li>
-            </ul>
-        </nav>
-    )
-}
+            window.scrollY > 50 ? setSticky(true) : setSticky(false);
+        });
+    }, []);
 
-export default Navbar
+    const toggleMenu = () => {
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    };
+
+    return (
+        <nav className={`container ${sticky ? "dark-nav" : ""}`}>
+            <img src={logo} alt="logo" className="logo" />
+            <ul className={mobileMenu ? "" : "mb-menu"}>
+                <li>
+                    <Link to="hero" smooth={true} offset={0} duration={500}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="program" smooth={true} offset={-260} duration={500}>
+                        Program
+                    </Link>
+                </li>
+                <li>
+                    <Link to="about" smooth={true} offset={-150} duration={500}>
+                        About
+                    </Link>{" "}
+                    US
+                </li>
+                <li>
+                    <Link to="campus" smooth={true} offset={-240} duration={500}>
+                        Campus
+                    </Link>
+                </li>
+                <li>
+                    <Link to="testimonials" smooth={true} offset={-260} duration={500}>
+                        Testimonials
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="contact"
+                        smooth={true}
+                        offset={-210}
+                        duration={500}
+                        className="btn"
+                    >
+                        Contact us
+                    </Link>
+                </li>
+            </ul>
+            <img
+                src={menuIcon}
+                alt="menu-icon"
+                className="menu-icon"
+                onClick={toggleMenu}
+            />
+        </nav>
+    );
+};
+
+export default Navbar;
